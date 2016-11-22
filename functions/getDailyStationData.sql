ce7114e85f461878f8ce129e8fc2e8418a421eb3
MUSE - Zoologia dei Vertebrati <musezoolvert@gmail.com>
2016-11-22 08:55:38 +0100

    Fixed bugs

--- functions/getDailyStationData.sql
+++ functions/getDailyStationData.sql
@@ -5,23 +5,36 @@ $BODY$
 	import json
 	import xmltodict
 
-	file = urllib2.urlopen('http://dati.meteotrentino.it/service.asmx/ultimiDatiStazione?codice=' + 'T0409')
+	file = urllib2.urlopen('http://dati.meteotrentino.it/service.asmx/ultimiDatiStazione?codice=' + param)
 	data = file.read()
 	file.close()
 	data = json.loads(json.dumps(xmltodict.parse(data)))
 	data = data['datiOggi']
-	temperatures = data['temperature']['temperatura_aria']
-	hasprec = False if not data['precipitazioni'] else True
-	haswind = False if not data['venti'] else True
 
+	idxs = []
+
+	# In case of no data in URL, returns a blank JSON
 	finaljson = {}
-	for index,item in enumerate(temperatures):
-		day = temperatures[index]['data']
-		finaljson[day] = {}
-		finaljson[day]['temperatura'] = temperatures[index]['temperatura'] 
-		finaljson[day]['rain'] = data['precipitazioni']['precipitazione'][index]['pioggia'] if hasprec else null
-		finaljson[day]['wind_int'] = data['venti']['vento_al_suolo']['v'] if haswind else ''
-		finaljson[day]['wind_dir'] = data['venti']['vento_al_suolo']['d'] if haswind else ''
+
+	if (False if not data['temperature'] else True):
+		idxs.append(len(list(data['temperature']['temperatura_aria'])))
+		temperatures = data['temperature']['temperatura_aria']
+
+		hasprec = False if not data['precipitazioni'] else True
+		if hasprec:
+			idxs.append(len(list(data['precipitazioni']['precipitazione'])))
+
+		haswind = False if not data['venti'] else True
+		if haswind:
+			idxs.append(len(list(data['venti']['vento_al_suolo'])))
+
+		for index in range(0,min(idxs)):
+			day = temperatures[index]['data']
+			finaljson[day] = {}
+			finaljson[day]['temperatura'] = temperatures[index]['temperatura'] 
+			finaljson[day]['rain'] = data['precipitazioni']['precipitazione'][index]['pioggia'] if hasprec else ''
+			finaljson[day]['wind_int'] = data['venti']['vento_al_suolo'][index]['v'] if haswind else ''
+			finaljson[day]['wind_dir'] = data['venti']['vento_al_suolo'][index]['d'] if haswind else ''
 
 	return json.dumps(finaljson)
 $BODY$
